959c59e99e6fe8fcf925677d9a98343ba215c393
Joel Moniz <jramoniz@gmail.com>
2015-08-10 01:21:09 +0530

    Update build properties, add license details

--- src/jm/mode/replmode/REPLEditor.java
+++ src/jm/mode/replmode/REPLEditor.java
@@ -1,3 +1,26 @@
+/*
+  An REPL/Live Coding Mode for Processing- https://github.com/joelmoniz/REPLmode
+  
+  A mode for Processing - http://processing.org
+  Developed during Google Summer of Code 2015
+  
+  Copyright (c) 2015 Joel Moniz
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, 
+  USA.
+ */
 package jm.mode.replmode;
 
 import java.awt.event.ActionEvent;
